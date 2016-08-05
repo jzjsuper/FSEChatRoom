93295826cdace857b36a1bff0b01d3a4ef210cb1
Zhijie Jiang <jacobjiang123@gmail.com>
2016-08-05 22:43:27 +0800

    Move socket logic to chat.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/chat', chat);
+app.use('/chat', chat(io));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/chat.js
+++ routes/chat.js
@@ -1,14 +1,12 @@
-var express = require('express');
-var router = express.Router();
+module.exports = function(io) {
+  var express = require('express');
+  var router = express.Router();
 
-router.all(function(req, res, next) {
-  console.log("GOTCHA");
-  next();
-});
+  /* GET home page. */
+  router.get('/', function(req, res, next) {
+    res.render('chat', { title: 'FSE Chat Room' , name: req.query.name});
+  });
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('chat', { title: 'FSE Chat Room' , name: req.query.name});
-});
+  return router;
 
-module.exports = router;
+}
